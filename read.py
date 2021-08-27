9204285fb3b6519ce7b22eee4262764119ceaef0
Chin Yu <chin.yuchiuj@gmail.com>
2021-08-27 11:47:34 +0800

    calc avg length of reviews

--- read.py
+++ read.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #count = count +1 
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('file has been read','it contains', len(data), 'data(s)')
 
-print(data[0])
-print('-------------')
-print(data[1])
+sum_len = 0 
+for d in data:
+	sum_len = sum_len + len(d) # sum_len += len(d)
+
+print('average comment len is', sum_len / len(data))
